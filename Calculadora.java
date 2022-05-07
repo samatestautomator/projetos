29744e3aee6e79d6fa60f9302dddfb6f6e655c2c
= <=>
2022-05-07 15:25:58 -0300

    Código dentro da classe main comentado e adicionado metodo soma

--- Calculadora.java
+++ Calculadora.java
@@ -3,13 +3,23 @@ class Calculadora{
 
 	public static void main (String args[]){
 
-	int soma;
-	int valor1 = 2;
-	int valor2 = 2;
+	// int soma;
+	// int valor1 = 2;
+	// int valor2 = 2;
 
-	soma = valor1 + valor2;
+	// soma = valor1 + valor2;
 
-    System.out.println("Resultado: "+soma);
+    // System.out.println("Resultado: "+soma);
+
+    soma(3,3);
+
+}
+
+
+public static void soma(int num1, int num2){
+    int soma = num1 + num2;
+
+    System.out.println(soma);
 }
 
 }
